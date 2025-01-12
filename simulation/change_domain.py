e53e15987e1c36288e106140a3d72d590d9c4dc5
FrancescoPesce <francesco1.pesce@mail.polimi.it>
2025-01-12 14:41:21 +0100

    Add results

--- simulation/change_domain.py
+++ simulation/change_domain.py
@@ -24,7 +24,7 @@ bevel_resolution = 10
 wire_section_radius = 0.5 / 100
 
 # Options for the domain.
-length_base_ratio = 10  # Ratio of length and base of the domain.
+length = 1.0  # Length of the domain in meters.
 num_copies = 4  # Number of copies of the grid before periodicity is applied.
 # Note that for the "grid" mesh, this is half the number of squares.
 """ ----------------- EDITABLE SETTINGS -----------------"""
@@ -34,7 +34,6 @@ num_copies = 4  # Number of copies of the grid before periodicity is applied.
 if mesh == "porous":
     domain_length = cavity_diameter + cavity_distancing
     base = domain_length * num_copies
-    length = length_base_ratio * base
     cavity_amount = num_copies + 2
     if slate_thickness > length:
         print("Error! Wrong parameters for porous mesh.")
@@ -46,7 +45,6 @@ else:
         sys.exit(1)
     domain_length = square_size * 2
     base = domain_length * num_copies
-    length = length_base_ratio * base
     square_amount = 2 * num_copies + 2
     if overlap_offset + wire_section_radius > length / 2:
         print("Error! Wrong parameters for grid mesh.")
@@ -57,20 +55,24 @@ else:
 # Create the desired mesh.
 filename = "constant/triSurface/grid.obj"
 if mesh == "porous":
-    os.system(f"$BLENDER_PATH --background --python ../mesh-generators/generate_porous_mesh.py -- \
+    os.system(
+        f"$BLENDER_PATH --background --python ../mesh-generators/generate_porous_mesh.py -- \
                --export_path {filename}                \
                --cavity_amount {cavity_amount}         \
                --cavity_diameter {cavity_diameter}     \
                --cavity_distancing {cavity_distancing} \
-               --slate_thickness {slate_thickness}")
+               --slate_thickness {slate_thickness}"
+    )
 else:
-    os.system(f"$BLENDER_PATH --background --python ../mesh-generators/generate_wire_mesh.py -- \
+    os.system(
+        f"$BLENDER_PATH --background --python ../mesh-generators/generate_wire_mesh.py -- \
                --export_path {filename}                \
                --square_size {square_size}             \
                --square_amount {square_amount}         \
                --overlap_offset {overlap_offset}       \
                --bevel_resolution {bevel_resolution}   \
-               --wire_section_radius {wire_section_radius}")
+               --wire_section_radius {wire_section_radius}"
+    )
 
 
 # Update domain parameters.
